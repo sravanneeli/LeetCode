f354c9e889464a3d349964dc2644523c98c94365
sravanneeli <46817445+sravanneeli@users.noreply.github.com>
2022-03-19 11:52:09 +0530

    Time: 178 ms (63.13%), Space: 15.1 MB (84.30%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,10 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        pres_dict = {}
-        for num in nums:
-            pres_dict[num] = True
-        for i in range(n+1):
-            if pres_dict.get(i) is None:
-                return i
-                
+        res = n * (n+1) // 2
+        for i in range(n):
+            res -= nums[i]
+        
+        return res
