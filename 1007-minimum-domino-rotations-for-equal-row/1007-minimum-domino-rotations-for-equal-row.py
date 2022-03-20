be7bd0cd1ff74a9d8c2a0a7f2b596a9a92f6477e
sravanneeli <46817445+sravanneeli@users.noreply.github.com>
2022-03-20 10:18:25 +0530

    Time: 1572 ms (43.66%), Space: 14.9 MB (94.32%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,28 +1,17 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        ans = float('inf')
-        for i in range(1, 7):
-            count = 0
-            for top, bottom in zip(tops, bottoms):
-                if top == i:
-                    continue
-                if bottom != i:
-                    count = float('inf')
-                    break
-                count += 1
-            ans = min(ans, count)
-            # print(ans)
+        tops_dict = {i: 0 for i in range(1, 7)}
+        bottoms_dict = {i: 0 for i in range(1, 7)}
+        same_dict = {i:0 for i in range(1, 7)}
+        for top, bottom in zip(tops, bottoms):
+            tops_dict[top] += 1    
+            bottoms_dict[bottom] += 1
+            if top == bottom:
+                same_dict[top] += 1
             
+        # print(same_dict)
+        n = len(tops)
         for i in range(1, 7):
-            count = 0
-            for bottom, top in zip(bottoms, tops):
-                if bottom == i:
-                    continue
-                if top != i:
-                    count = float('inf')
-                    break
-                count += 1
-            ans = min(ans, count)
-        
-        return ans if ans != float('inf') else -1
-                    
+            if tops_dict[i] + bottoms_dict[i] - same_dict[i] == n:
+                return n - max(tops_dict[i], bottoms_dict[i])
+        return -1
