4319302acbc1dfa077f1cec86e6ad8581996db57
sravanneeli <46817445+sravanneeli@users.noreply.github.com>
2022-04-11 12:29:29 +0530

    Time: 58 ms (50.11%), Space: 14 MB (95.42%) - LeetHub

--- 682-baseball-game/682-baseball-game.py
+++ 682-baseball-game/682-baseball-game.py
@@ -3,15 +3,13 @@ class Solution:
         stck = []
         
         for char in ops:
-            if char.isnumeric():
-                stck.append(int(char))
-            elif char[0] == '-':
-                stck.append(-int(char[1:]))
-            elif char == "+":
+            if char == "+":
                 stck.append(stck[-1] + stck[-2])
             elif char == "D":
                 stck.append(stck[-1] * 2)
             elif char == "C":
                 stck.pop()
+            else:
+                stck.append(int(char))
             # print(stck)
         return sum(stck)
